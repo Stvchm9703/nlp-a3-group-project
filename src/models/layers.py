d8e44ea932a92391af0d5667bd707b6f6f9528b0
Steven CHM <stv.chm@gmail.com>
2024-05-01 22:23:19 +1000

    snap

---
+++ src/models/layers.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def positional_encoding(length, depth):
+    depth = depth/2
+
+    positions = np.arange(length)[:, np.newaxis]     # (seq, 1)
+    depths = np.arange(depth)[np.newaxis, :]/depth   # (1, depth)
+
+    angle_rates = 1 / (10000**depths)         # (1, depth)
+    angle_rads = positions * angle_rates      # (pos, depth)
+
+    pos_encoding = layers.concatenate(
+        [tf.math.sin(angle_rads), tf.math.cos(angle_rads)],
+        axis=-1)
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+class PositionalEmbedding(layers.Layer):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.d_model = d_model
+        self.embedding = layers.Embedding(vocab_size, d_model, mask_zero=True)
+        self.pos_encoding = positional_encoding(length=2048, depth=d_model)
+
+    def compute_mask(self, *args, **kwargs):
+        return self.embedding.compute_mask(*args, **kwargs)
+
+
+    def call(self, x):
+        length = tf.shape(x)[1]
+        x = self.embedding(x)
+        # This factor sets the relative scale of the embedding and positonal_encoding.
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x = x + self.pos_encoding[tf.newaxis, :length, :]
+        return x
+
+
+class BaseAttention(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = layers.MultiHeadAttention(**kwargs)
+        self.layernorm = layers.LayerNormalization()
+        self.add = layers.Add()
+
+
+
+
+class CrossAttention(BaseAttention):
+    def call(self, x, context):
+        attn_output, attn_scores = self.mha(
+            query=x,
+            key=context,
+            value=context,
+            return_attention_scores=True)
+
+        # Cache the attention scores for plotting later.
+        self.last_attn_scores = attn_scores
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+class GlobalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha( query=x, value=x, key=x)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+class CausalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha(
+            query=x,
+            value=x,
+            key=x,
+            use_causal_mask = True)
+        x = self.add([x, attn_output])
+        x = self.layernorm(x)
+        return x
+
+class FeedForward(layers.Layer):
+    def __init__(self, d_model, dff, dropout_rate=0.1):
+        super().__init__()
+        self.seq = models.Sequential([
+            layers.Dense(dff, activation='relu'),
+            layers.Dense(d_model),
+            layers.Dropout(dropout_rate)
+        ])
+        self.add = layers.Add()
+        self.layer_norm = layers.LayerNormalization()
+
+    def call(self, x):
+        x = self.add([x, self.seq(x)])
+        x = self.layer_norm(x)
+        return x
